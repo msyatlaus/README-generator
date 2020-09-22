274f9a00cb5cdb3c1846b1019ef21f7f28af6f31
Bradley Guidry <bradleyguidry@gmail.com>
2020-09-22 16:00:56 -0500

    connects gitgub

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // function to generate markdown for README
 function generateReadme(data) {
-    const avatar = `https://github.com/${data.username}.png?size=50`;
-    const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbbb9`;
+    const avatar = `https://github.com/${data.github}.png?size=50`;
+    const gitHub = `https://img.shields.io/badge/Github-${data.github}-4cbbb9`;
 
     return `
     # ${data.title}
